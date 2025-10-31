e05eb52f87ee422460886db41593989d1a4b97ea
Nathaniel E Adams <47441281+nadamspueblo@users.noreply.github.com>
2025-10-31 21:49:39 +0000

    Implemented add task functionality

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 console.log("Script started");
+
+// Add a task
+function addTask() {
+    /*
+    <div id="task1" class="task-item">
+        <input type="checkbox" id="checkbox1">
+        <label id="label1">Take out trash</label>
+    </div>
+    */
+
+    // Get task text
+    let textbox = document.getElementById("task-input");
+    let taskText = textbox.value;
+    textbox.value = "";
+
+    // Get tasklist 
+    let taskList = document.getElementById("task-list");
+
+    // Generate id number
+    let idNum = taskList.childElementCount;
+
+    // Create task div
+    let taskDiv = document.createElement("div");
+    taskDiv.id = "task" + idNum;
+    taskDiv.classList.add("task-item");
+    if (idNum % 2 == 1) {
+        taskDiv.style.backgroundColor = "turquoise";
+    }
+
+    // Create checkbox
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "checkbox" + idNum;
+
+    // Create label
+    let label = document.createElement("label");
+    label.id = "label" + idNum;
+
+    // Set label text
+    label.innerText = taskText;
+
+    // Add the checkbox the task div
+    taskDiv.appendChild(checkbox);
+
+    // Add the label to the task div
+    taskDiv.appendChild(label);
+
+    // Add the task div to the list
+    taskList.appendChild(taskDiv);
+}
